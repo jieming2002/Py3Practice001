a77f949024b276c211333b1986f3564793108e67
jieming2002 <jieming2002@gmail.com>
2017-11-22 18:09:16 +0800

    os

---
+++ py3018OS.py
@@ -0,0 +1,32 @@
+__author__ = 'skye 2017-11-22'
+# coding=utf-8
+
+import os
+
+def searchFile(startDir, target):
+    os.chdir(startDir)
+
+    for file in os.listdir(os.curdir):
+        ext = os.path.splitext(file)[1]
+        if ext in target:
+            fileList.append(os.getcwd() + os.sep + file + os.linesep)
+        if os.path.isdir(file):
+            searchFile(file, target)    # 递归调用
+            os.chdir(os.pardir)         # 递归调用后切记返回上一层目录
+
+startDir = input('请输入待查找的初始目录：')
+workDir = os.getcwd() #当前工作目录
+
+target = ['.txt', '.jpg', '.pkl', '.png']
+fileList = []
+searchFile(startDir, target)
+
+f = open(workDir + os.sep + 'data' + os.sep + 'fileList.txt', 'w')
+f.writelines(fileList)
+f.close()
+
+print() ' '
+
+a ) b j &
+
+
---
+++ py3019Error.py
@@ -0,0 +1,5 @@
+__author__ = 'skye 2017-11-22'
+# coding=utf-8
+
+
+
